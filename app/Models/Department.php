0c9f29820eeff9b2242f788440d464a96f7a35f5
htweko01 <htweko9925@gmail.com>
2025-01-25 20:03:57 +0630

    create project, install filament and create Brand, Department, Category migrations

---
+++ app/Models/Department.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Department extends Model
+{
+    //
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    protected $fillable = ['name', 'slug', 'image'];
+}
